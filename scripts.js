1d481bb22b4119e3447f2660b3164bec0ba843a8
handrew335 <andreshg333@gmail.com>
2023-10-09 16:50:42 -0600

    Added playRound wich gives the result of the match

--- scripts.js
+++ scripts.js
@@ -2,15 +2,57 @@ function getComputerChoice(resultado) {
     const randomNum = Math.floor(Math.random() * 3 + 1);
 
     if (randomNum == 1) {
-        resultado == console.log("Rock");
+        result = "rock";
     }
     else if (randomNum == 2) {
-        resultado == console.log("Paper");
+        result = "paper";
     }
     else {
-        resultado == console.log("Scissors");
+        result = "scissors";
     }
-    return resultado;
-  }
-  console.log(getComputerChoice())
+    return result;
+}
+  
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  let result = "";
 
+  if (playerSelection == "rock") {
+    if (computerSelection == "rock") {
+      result = "Tie!";
+    }
+    else if (computerSelection == "paper"){
+      result = "Lose!";
+    }
+    else {
+      result = "Win!";
+    }
+  }
+  else if (playerSelection == "paper") {
+    if (computerSelection == "rock") {
+      result = "Win!";
+    }
+    else if (computerSelection == "paper"){
+      result = "Tie!";
+    }
+    else {
+      result = "Lose!";
+    }
+  }
+  else if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+      result = "Lose!";
+    }
+    else if (computerSelection == "paper"){
+      result = "Win!";
+    }
+    else {
+      result = "Tie!";
+    }
+  }
+  return result;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
